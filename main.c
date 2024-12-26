4d37413375bc0ff3788cf3d3ad5c1c6351d89e47
Sergei <kvt500@gmail.com>
2024-12-26 23:58:21 +0500

    ttyy

--- main.c
+++ main.c
@@ -3,7 +3,11 @@
 int main(){
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     printf("Hello World 6!")
+=======
+    printf("Hello World 4!")
+>>>>>>> parent of a703fb3 (eee)
 =======
     printf("Hello World 4!")
 >>>>>>> parent of a703fb3 (eee)
